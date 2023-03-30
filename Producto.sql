59dffefebca21b01f2878325ac2401245f77cf5f
ElevierLeon <18170367@itculiacan.edu.mx>
2023-03-30 09:54:51 -0700

    Paso 3, agregar un select * producto y select * producto by id

--- Producto.sql
+++ Producto.sql
@@ -1,5 +1,6 @@
-create table Carros (
+create table Producto (
     id int not null auto_increment primary key,
     marca nvarchar(20) not null,
-    nombre nvarchar(20) not null
+    nombre nvarchar(20) not null,
+    precio nvarchar(20) not null
 )
--- index.js
+++ index.js
@@ -3,6 +3,49 @@ const app = express();
 const port =3000;
 app.use(express.json());
 
+const mysql = require('mysql');
+
+var con =mysql.createConnection({
+    host: "localhost",
+    user: "root",
+    password: "example",
+    database: "Producto"
+});
+
+con.connect(function (err){
+    if (err) throw err;
+    console.log("Connected");
+});
+
+
+
+app.get('/producto', (req,res) => {
+    con.query("Select * from Producto",
+        function(err,result){
+            if(err){
+                throw err;
+            }
+            res.status(200).json(result);
+        }
+    );
+});
+
+
+app.get('/producto/:id', (req,res)=>{
+    let id=req.params.id ?? 0;
+    con.query("Select * from Producto where id=?", [id],
+    function(err,result){
+        if(err){
+            throw err;
+        }
+        if(!!result && result.length>0){
+            res.status(200).json(result);
+        } else {
+            res.status(400).json({});
+        }
+
+    });
+} );
 
 app.listen(port,() => {
     console.log(`Example app listening on port ${port}`);
